d5870d4e838898eb98db4819739dbff547b4f8b1
cruzmiyagi26 <kountry.nelson@gmail.com>
2022-04-26 12:26:31 -0400

    5th

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -45,7 +45,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner">
                         <div className="desc">
                           {/* <h1>I often <br/>Write ... </h1> */}
-                          <p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
                         </div>
                       </div>
                     </div>
